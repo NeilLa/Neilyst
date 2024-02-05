ee808e4a33ea47939300616d5b1d817743785f4b
neil <597647511@qq.com>
2024-02-05 16:52:33 +0800

    delete: some useless from main branch

--- Neilyst/Neilyst.py
+++
@@ -1,5 +0,0 @@
-from Neilyst.Strategy import Strategy
-
-class Neilyst(Strategy):
-    def __init__(self, exchange_name) -> None:
-        super().__init__(exchange_name)
--- Neilyst/__init__.py
+++ Neilyst/__init__.py
@@ -3,3 +3,5 @@ from Neilyst.data import get_klines
 from Neilyst.analyze import load_history, calculate_win_rate
 
 from Neilyst.backtest import backtest
+
+from Neilyst.models import strategy, signal

02cee2fa59f01d9b2cf75897205aca9ca63f9ca0
neil <597647511@qq.com>
2024-09-04 16:07:52 +0800

    can support multi symbol data now

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 # 本文件用于定义一些通用类
-from abc import ABC, abstractclassmethod
+from abc import ABC
 from pandas import Timestamp
 import pandas as pd
 
@@ -11,38 +11,49 @@ class Strategy(ABC):
         self.data = data
         self.indicators = indicators
 
-    def get_recent_data(self, date, periods, data=None, indicators=None):
-        # 根据引擎传入的date，找到最近的N条数据
+    def get_recent_data(self, date, periods, data=None, indicators=None, symbol=None):
+        """
+        获取最近的 N 条数据和指标。
+        如果传入的 data 和 indicators 是 dict则按 symbol 处理；
+        如果传入的是 DataFrame则直接处理。
+        """
         if not isinstance(date, Timestamp):
             date = Timestamp(date)
-        
-        if (data is None or data.empty) and (indicators is None or indicators.empty):
-            data = self.data
-            indicators = self.indicators
+
+        # 如果传入的数据是 dict，按照 symbol 获取数据
+        if isinstance(data, dict) and isinstance(indicators, dict):
+            if symbol is None:
+                raise ValueError("Symbol must be provided for multi-symbol data.")
+            
+            # 获取指定 symbol 的数据和指标
+            data = self.data[symbol] if data is None else data[symbol]
+            indicators = self.indicators[symbol] if indicators is None else indicators[symbol]
         else:
-            data = data
-            indicators = indicators
-        # 找到数据中最近的一条数据的位置
+            # 如果不是 dict，直接使用传入的 DataFrame
+            data = self.data if data is None else data
+            indicators = self.indicators if indicators is None else indicators
+
+        # 找到最近的 N 条数据的位置
         recent_data_idx = data.index[data.index <= date][-periods:]
         recent_indicators_idx = indicators.index[indicators.index <= date][-periods:]
 
+        # 获取最近的 N 条数据和指标
         recent_data = data.loc[recent_data_idx]
         recent_indicators = indicators.loc[recent_indicators_idx]
-        
-        # 合并
+
+        # 合并数据和指标
         recent_combined = pd.concat([recent_data, recent_indicators], axis=1, join='inner')
 
         return recent_combined
 
-    # run方法每次接收一行1min级别数据用作驱动
-    # 以及当前仓位信息, 应该包含开仓价，仓位多少，浮盈浮亏
-    # 其余所需数据可以有自己去get_kline以及计算
-    # 返回一个对象
-    # 对象应该包含开仓方向(long, short, close)
-    # 还应该包含开仓价（这个价格自己根据传入的close计算），以及开仓量
-    # 如果返回None，则不做任何操作
-    @abstractclassmethod
-    def run(self, date, price_row, current_pos, current_balance):
+    def run(self, date, price_row, current_pos, current_balance, symbol):
+        # run方法每次接收一行1min级别数据用作驱动
+        # 以及当前仓位信息, 应该包含开仓价，仓位多少，浮盈浮亏
+        # 其余所需数据可以有自己去get_kline以及计算
+        # 返回一个对象
+        # 对象应该包含开仓方向(long, short, close)
+        # 还应该包含开仓价（这个价格自己根据传入的close计算），以及开仓量
+        # 如果返回None，则不做任何操作
         pass
 
 class Signal():
